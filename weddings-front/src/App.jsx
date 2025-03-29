1c29c5178d13d5af1467eb58b636d75b26c2c135
aswinraj0919 <aswinraj.stackup@gmail.com>
2025-03-29 17:51:28 +0530

    by aswin

--- weddings-front/src/App.jsx
+++ weddings-front/src/App.jsx
@@ -1,3 +1,5 @@
+import VenueCards from './components/VenueCards';
+import WeddingStyles from './components/WeddingStyles';
 import Connect from './components/Connect';
 import Footer from './components/Footer';
 import './App.css'
@@ -5,6 +7,8 @@ import './App.css'
 function App() {
   return (
     <div className="app">
+      <VenueCards />
+      <WeddingStyles />
       <Connect />
       <Footer />
     </div>
